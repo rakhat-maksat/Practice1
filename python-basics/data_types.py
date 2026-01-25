7bb2c9f24df55e1c635afd668531e2a21edc7abe
rakhat-maksat <rm30052008@gmail.com>
2026-01-25 20:58:25 +0500

    Add Pratice1 Python basics example

--- python-basics/data_types.py
+++ python-basics/data_types.py
@@ -6,3 +6,33 @@ x = "Hello World"
 print(x)
 #display the data type of x:
 print(type(x)) 
+
+x = "Hello World"
+#display x:
+print(x)
+#display the data type of x:
+print(type(x)) 
+
+x = 20
+#display x:
+print(x)
+#display the data type of x:
+print(type(x)) 
+
+x = 20.5
+#display x:
+print(x)
+#display the data type of x:
+print(type(x)) 
+
+x = 1j
+#display x:
+print(x)
+#display the data type of x:
+print(type(x)) 
+
+x = ["apple", "banana", "cherry"]
+#display x:
+print(x)
+#display the data type of x:
+print(type(x)) 
